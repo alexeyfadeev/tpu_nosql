45d30d6c2654ef62b7a4787cd25189f6ab35b21f
Alexey Fadeev <fadeev@atis-process.com>
2016-04-10 14:58:50 +0600

    Xml To Json Converter

---
+++ XmlToJson/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Xml;
+using Newtonsoft.Json;
+
+namespace Tpu.NoSql.XmlToJson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Count() < 1) return;
+            var files = Directory.GetFiles(args[0], "*.xml");
+
+            foreach (string file in files)
+            {
+                XmlDocument doc = new XmlDocument();
+                doc.Load(file);
+                string json = JsonConvert.SerializeXmlNode(doc);
+
+                string fileJson = file.ToLower().Replace(".xml", ".json");
+                using (var streamWriter = new StreamWriter(fileJson, false, Encoding.UTF8))
+                {
+                    streamWriter.Write(json);
+                }
+            }
+        }
+    }
+}
